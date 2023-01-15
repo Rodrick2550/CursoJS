d11faf134475191d30f38b53fd5aa4d196a3245f
rodrick <213480@ids.upchiapas.edu.mx>
2023-01-15 15:50:29 -0600

    second excercise

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('hola mundo')
+
+
+const rodrigo = {
+    "name": 'Rodrigo',
+    "age": 19,
+    "are you dev?": false,
+    "date of birth": new Date('august 08 2003'),
+    "favorite book": {
+        " title": 'Dairy of a wimpy kid',
+        "author": 'Jeff Kinney',
+        "date": new Date('april 1 2007'),
+        "url": 'https://en.wikipedia.org/wiki/Diary_of_a_Wimpy_Kid'
+    }
+
+}
+
+console.log(rodrigo)
