efd2da42e06dd2509c516d9473449c4a52d4ca98
rodrick <213480@ids.upchiapas.edu.mx>
2023-01-22 02:14:18 -0600

    excercice 5

---
+++ Ejercicio5.js
@@ -0,0 +1,8 @@
+let altura = 170;
+let alturaMetros = 1.70;
+let peso = 65.80;
+let alturaredondeo = Math.ceil(alturaMetros)
+console.log(alturaredondeo)
+let pesoredondeado = Math.floor(peso)
+console.log(pesoredondeado)
+let maximo = console.log('El maximo valor en JavaScript es: '+Number.MAX_VALUE)
